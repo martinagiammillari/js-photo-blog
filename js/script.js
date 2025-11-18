1fd57569a8a71fdd671a7fbd80ed4607f5f83489
martinagiammillari <martigiammi@gmail.com>
2025-11-18 13:52:48 +0100

    feat: elementi del dom selezionati

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+
+
+// SELEZIONARE GLI ELEMENTI DEL DOM
+const container = document.getElementById("container");
+
+
+
+
+
+// FARE CHIAMATA API PER PRENDERE I DATI
+const API = "https://lanciweb.github.io/demo/api/pictures/"
+
+
+
+
+
+// CREARE DINAMICAMENTE LE CARD
+
+// STAMPARE IN PAGINA
+
+// REFACTORING DEL CODICE
+
