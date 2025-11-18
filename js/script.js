63560eb36f32e31a9c3656c116940bd97d81d962
martinagiammillari <martigiammi@gmail.com>
2025-11-18 14:14:55 +0100

    feat: stampato in pagina

--- js/script.js
+++ js/script.js
@@ -1,23 +1,37 @@
 
-
-
 // SELEZIONARE GLI ELEMENTI DEL DOM
 const container = document.getElementById("container");
 
-
-
-
-
 // FARE CHIAMATA API PER PRENDERE I DATI
-const API = "https://lanciweb.github.io/demo/api/pictures/"
+const API = "https://lanciweb.github.io/demo/api/pictures/";
+
+axios.get(API).then(function (resp) {
+    const cardArray = resp.data;
+    // CREARE DINAMICAMENTE LE CARD
+
+    let cardStr = "";
+
+    cardArray.forEach(function (curCard) {
+        cardStr += `
+            <div class="card">
+                <img class="pin" src="./img/pin.svg" alt="">
+                <img src="${curCard.url}" alt="${curCard.title}">
+                <div class="card-text">
+                    <p>${curCard.date}<br>
+                    <span>${curCard.title}</span></p>
+                </div>
+            </div>
+        `;
+    });
+
+    // STAMPARE IN PAGINA
+    container.innerHTML = cardStr;
+});
 
 
 
 
 
-// CREARE DINAMICAMENTE LE CARD
-
-// STAMPARE IN PAGINA
 
 // REFACTORING DEL CODICE
 
