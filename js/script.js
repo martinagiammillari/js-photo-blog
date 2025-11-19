00b8e11e020ea01a1d5c1c465559a76a4e8ddf7a
martinagiammillari <martigiammi@gmail.com>
2025-11-19 14:21:32 +0100

    feat: milestone 2 e 3 done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-
 // SELEZIONARE GLI ELEMENTI DEL DOM
 const container = document.getElementById("container");
+const overlay = document.getElementById("overlay");
+const overlayImg = document.getElementById("overlayImg");
+const closeBtn = document.getElementById("closeBtn");
 
 // FARE CHIAMATA API PER PRENDERE I DATI
 const API = "https://lanciweb.github.io/demo/api/pictures/";
@@ -16,7 +18,7 @@ axios.get(API).then(function (resp) {
         cardStr += `
             <div class="card rotate">
                 <img class="pin" src="./img/pin.svg" alt="">
-                <img src="${curCard.url}" alt="${curCard.title}">
+                <img class="photo" src="${curCard.url}" alt="${curCard.title}">
                 <div class="card-text">
                     <p class="date">${curCard.date}</p>
                     <span>${curCard.title.toUpperCase()}</span>
@@ -27,14 +29,32 @@ axios.get(API).then(function (resp) {
 
     // STAMPARE IN PAGINA
     container.innerHTML = cardStr;
+}).catch(function (error) {
+    console.error("Errore nel caricamento delle immagini:", error);
 });
 
 
+// APRI OVERLAY QUANDO CLICCHI UNA FOTO
+
+    const allPhotos = document.querySelectorAll(".photo");
+
+    // PER OGNI FOTO AGGIUNGO IL CLICK
+    allPhotos.forEach(function (photo) {
+
+        photo.addEventListener("click", function () {
+
+            // IMPOSTO L’IMMAGINE NELL’OVERLAY
+            overlayImg.src = photo.src;
+
+            // MOSTRO L’OVERLAY
+            overlay.classList.add("active");
+        });
+    });
 
 
 
 
-
-
-// REFACTORING DEL CODICE
-
+// BOTTONE PER CHIUDERE
+closeBtn.addEventListener("click", function () {
+    overlay.classList.remove("active");
+});
